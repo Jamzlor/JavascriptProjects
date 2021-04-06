93610434bcac55d14993cb526ef7cf32f5f0cfe9
Jamzlor <jameslo.dev@gmail.com>
2021-04-06 18:46:29 +0800

    completed all features in this app

---
+++ js-counter/app.js
@@ -0,0 +1,20 @@
+let numDisplay = document.getElementById('numDisplay')
+let count = 1;
+
+numDisplay.innerHTML = count;
+
+let increaseButton = document.getElementById('increaseCount');
+let decreaseButton = document.getElementById('decreaseCount');
+
+function plusOne (){
+    count += 1;
+    numDisplay.innerHTML = count;
+};
+
+function minusOne (){
+    count -= 1;
+    numDisplay.innerHTML = count;
+};
+
+increaseButton.addEventListener('click', plusOne);
+decreaseButton.addEventListener('click', minusOne);
