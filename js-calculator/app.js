5dcbcc8220fe8cb7f9d23ed5bfaa2ed4a694cf2c
Jamzlor <jameslo.dev@gmail.com>
2021-04-20 14:46:19 +0800

    simple function are completed

--- js-calculator/app.js
+++ js-calculator/app.js
@@ -9,28 +9,52 @@ const equals = document.getElementById('equal');
 
 
 var equation = [];
-let input = '';
+var input = '';
+var evaluated = false;
 
 // keyNum input functions
 Array.from(keyNum).forEach(button => button.addEventListener('click', function(){
-    if(input.length === 0){
+    if(evaluated){
+        input = '';
+        equation = [];
         display.innerHTML = '';
-        display.innerHTML += button.innerHTML;
-        input += button.innerHTML;
-    } else if(display.innerHTML.length < 25){
-        input += button.innerHTML;
-        display.innerHTML += button.innerHTML;    
+        console.log(input)
+        console.clear();
+        if(input.length === 0){
+            display.innerHTML = '';
+            display.innerHTML += button.innerHTML;
+            input += button.innerHTML;
+        } else if(display.innerHTML.length < 25){
+            input += button.innerHTML;
+            display.innerHTML += button.innerHTML;    
+        }
+        evaluated = false;
+    } else {
+        if(input.length === 0){
+            display.innerHTML = '';
+            display.innerHTML += button.innerHTML;
+            input += button.innerHTML;
+        } else if(display.innerHTML.length < 25){
+            input += button.innerHTML;
+            display.innerHTML += button.innerHTML;    
+        }
     }
+    
+    //debugging
     console.log(input);
+    console.log(equation)
 }));
 
 // operators input function
 Array.from(operators).forEach(button => button.addEventListener('click', function(){
     if(input.length > 0){
         equation.push(input);
-        equation.push(button.innerHTML);
         input = '';
+        equation.push(button.innerHTML);
     }
+
+    //debugging
+    console.log(input);
     console.log(equation);
 }));
 
@@ -43,7 +67,20 @@ decimal.addEventListener('click', function(){
         input += '.';
         display.innerHTML += '.';
     }
+
+    //debugging
     console.log(input)
+});
+
+//plusminus input function
+plusMinus.addEventListener('click', function(){
+    if(input[0] === '-'){
+        input = input.slice(1, input.length);
+        display.innerHTML = input;
+    } else {
+        input = '-' + input;
+        display.innerHTML = input;
+    }
 });
 
 // equal input function
@@ -51,5 +88,24 @@ equals.addEventListener('click', function(){
     equation.push(input);
     input = '';
     let result = eval(equation.join(''));
-    display.innerHTML = result;
+    display.innerHTML = (result === undefined) ? 0 : result;
+    evaluated = true;
+
+    //debugging
+    console.log(result)
+    console.log(input)
+    console.log(equation)
 });
+
+// clear input function
+clear.addEventListener('click', function(){
+    input = '';
+    equation = [];
+    display.innerHTML = '';
+    evaluated = false;
+
+    //debugging
+    console.log(input)
+    console.clear();
+    
+})
